787eedad9649f132499646d7ad0d537615863fc9
kushalshrestha <kushalshr@gmail.com>
2025-03-31 00:01:42 -0700

    feat: test

--- 97. Streaming in Pyspark_Project/streaming_processor.py
+++ 97. Streaming in Pyspark_Project/streaming_processor.py
@@ -12,59 +12,59 @@ spark = SparkSession.builder \
     .config("spark.driver.extraClassPath", "/opt/spark/jars/spark-sql-kafka-0-10_2.12-3.1.1.jar") \
     .getOrCreate()
 
+spark.streams.awaitAnyTermination()
+
+# # Define schema for incoming JSON data
+# schema = StructType() \
+#     .add("user_id", IntegerType()) \
+#     .add("event_type", StringType()) \
+#     .add("timestamp", DoubleType())
+
+# # Read from Kafka topic as a streaming DataFrame
+# df_stream = spark.readStream \
+#     .format("kafka") \
+#     .option("kafka.bootstrap.servers", "kafka:9092") \
+#     .option("subscribe", "real_time_events") \
+#     .option("startingOffsets", "earliest") \
+#     .load()
+
+# # Deserialize JSON data
+# df_parsed = df_stream.selectExpr("CAST(value AS STRING)") \
+#     .select(from_json(col("value"), schema).alias("data")) \
+#     .select("data.*")
+
+# # Perform Aggregation - Count Events per Window
+# df_aggregated = df_parsed \
+#     .withWatermark("timestamp", "10 minutes") \
+#     .groupBy(
+#         window(col("timestamp").cast("timestamp"), "5 minutes"),  # 5-minute window
+#         col("event_type")
+#     ).count()
+
+# # Perform Aggregation - Count Events per Window
+# df_aggregated = df_parsed \
+#     .withWatermark("timestamp", "10 minutes") \
+#     .selectExpr("CAST(timestamp AS TIMESTAMP) as timestamp", "event_type") \
+#     .groupBy(
+#         window(col("timestamp"), "5 minutes"),  # 5-minute window
+#         col("event_type")
+#     ).count()
 
 
-# Define schema for incoming JSON data
-schema = StructType() \
-    .add("user_id", IntegerType()) \
-    .add("event_type", StringType()) \
-    .add("timestamp", DoubleType())
 
-# Read from Kafka topic as a streaming DataFrame
-df_stream = spark.readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "kafka:9092") \
-    .option("subscribe", "real_time_events") \
-    .option("startingOffsets", "earliest") \
-    .load()
+# # Write output to PostgreSQL
+# query = df_aggregated.writeStream \
+#     .outputMode("update") \
+#     .foreachBatch(lambda batch_df, batch_id: batch_df.write
+#         .format("jdbc")
+#         .option("url", "jdbc:postgresql://postgres:5432/yourdb")
+#         .option("dbtable", "event_counts")
+#         .option("user", "user")
+#         .option("password", "password")
+#         .option("driver", "org.postgresql.Driver")
+#         .mode("append")
+#         .save()
+#     ) \
+#     .start()
 
-# Deserialize JSON data
-df_parsed = df_stream.selectExpr("CAST(value AS STRING)") \
-    .select(from_json(col("value"), schema).alias("data")) \
-    .select("data.*")
-
-# Perform Aggregation - Count Events per Window
-df_aggregated = df_parsed \
-    .withWatermark("timestamp", "10 minutes") \
-    .groupBy(
-        window(col("timestamp").cast("timestamp"), "5 minutes"),  # 5-minute window
-        col("event_type")
-    ).count()
-
-# Perform Aggregation - Count Events per Window
-df_aggregated = df_parsed \
-    .withWatermark("timestamp", "10 minutes") \
-    .selectExpr("CAST(timestamp AS TIMESTAMP) as timestamp", "event_type") \
-    .groupBy(
-        window(col("timestamp"), "5 minutes"),  # 5-minute window
-        col("event_type")
-    ).count()
-
-
-
-# Write output to PostgreSQL
-query = df_aggregated.writeStream \
-    .outputMode("update") \
-    .foreachBatch(lambda batch_df, batch_id: batch_df.write
-        .format("jdbc")
-        .option("url", "jdbc:postgresql://postgres:5432/yourdb")
-        .option("dbtable", "event_counts")
-        .option("user", "user")
-        .option("password", "password")
-        .option("driver", "org.postgresql.Driver")
-        .mode("append")
-        .save()
-    ) \
-    .start()
-
-query.awaitTermination()
+# query.awaitTermination()
